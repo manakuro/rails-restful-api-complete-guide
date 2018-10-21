3a33c9cfb6aaf83b38ec7d4008b0741e6d1d39d4
manato kuroda <a7108100@gmail.com>
2018-10-21 21:09:29 +0900

    feat: add update in articles

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -124,4 +124,94 @@ describe ArticlesController do
       end
     end
   end
+
+  describe '#update' do
+    let(:article) { create :article }
+
+    subject { patch :update, params: { id: article.id } }
+
+    context 'when no code provided' do
+      it_behaves_like 'forbidden_requests'
+    end
+
+    context 'when invalid code provided' do
+      before { request.headers['authorization'] = 'invalid token' }
+      it_behaves_like 'forbidden_requests'
+    end
+
+    context 'when authorized' do
+      let(:user) { create :user }
+      let(:access_token) { create :access_token, user: user }
+
+      before { request.headers['authorization'] = "Bearer #{access_token.token}" }
+
+      context 'when invalid parameters provided' do
+        let(:invalid_attributes) do
+          {
+            data: {
+              attributes: {
+                title: '',
+                content: ''
+              }
+            }
+          }
+        end
+        subject { post :update, params: invalid_attributes.merge(id: article.id) }
+
+        it 'should return 422 status code' do
+          subject
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+
+        it 'should return proper error json' do
+          subject
+          expect(json['errors']).to include(
+            {
+              "source" => { "pointer" => "/data/attributes/title" },
+              "detail" => "can't be blank"
+            },
+            {
+              "source" => { "pointer" => "/data/attributes/content" },
+              "detail" => "can't be blank"
+            })
+
+        end
+      end
+    end
+
+    context 'when success request sent' do
+      let(:user) { create :user }
+      let(:access_token) { create :access_token, user: user }
+
+      before { request.headers['authorization'] = "Bearer #{access_token.token}" }
+
+      let(:valid_attributes) do
+        {
+          'data' => {
+            'attributes' => {
+              'title' => 'title',
+              'content' => 'content',
+            }
+          }
+        }
+      end
+
+      subject { post :update, params: valid_attributes.merge(id: article.id) }
+
+      it 'should have 201 status code' do
+        subject
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'should have proper body' do
+        subject
+        expect(json_data['attributes']).to include(valid_attributes['data']['attributes'])
+      end
+
+      it 'should update the article' do
+        subject
+        expect(article.reload.title).to eq(valid_attributes['data']['attributes']['title'])
+      end
+    end
+  end
 end
