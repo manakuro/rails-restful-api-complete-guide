bc18add57632d5970e0dbc2f2a043b826aee13b7
manato kuroda <a7108100@gmail.com>
2018-10-22 17:47:33 +0900

    feat: finish Creating comments improvements

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,12 @@
 class CommentsController < ApplicationController
   skip_before_action :authorize!, only: [:index]
-  before_action :load_article, only: %i(create)
+  before_action :load_article
 
   # GET /comments
   def index
-    @comments = Comment.all
+    comments = @article.comments.page(params[:page]).per(params[:per_page])
 
-    render json: @comments
+    render json: comments
   end
 
 
@@ -14,11 +14,12 @@ class CommentsController < ApplicationController
   def create
     @comment = @article.comments.build(comment_params.merge(user: current_user))
 
-    if @comment.save
-      render json: @comment, status: :created, location: @article
-    else
-      render json: @comment.errors, status: :unprocessable_entity
-    end
+    @comment.save!
+    render json: @comment, status: :created, location: @article
+  rescue
+    render json: @comment, adapter: :json_api,
+           serializer: ErrorSerializer,
+           status: :unprocessable_entity
   end
 
   private
@@ -27,8 +28,8 @@ class CommentsController < ApplicationController
     @article = Article.find(params[:article_id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def comment_params
-    params.require(:comment).permit(:content)
+    params.require(:data).require(:attributes).permit(:content) ||
+      ActionController::Parameters.new
   end
 end
